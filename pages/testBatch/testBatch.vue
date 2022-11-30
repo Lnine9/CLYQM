6c8c5c8c42da5186aed99bcfef09bf66444b5034
Sakura <582117094@qq.com>
2022-11-30 14:28:15 +0800

    修改搜索逻辑

--- pages/testBatch/testBatch.vue
+++ pages/testBatch/testBatch.vue
@@ -1,66 +1,66 @@
 <template>
 	<view>
 		<u-sticky>
-		<view class="topInfo">
-			<view class="title">{{batchName}}</view>
-			<u-row gutter="20" customStyle="margin-bottom: 5%;margin-top: 5%">
-				<u-col span="6">
-					<u-row>
-						<u-col span="3">
-							<text>楼栋</text>
-						</u-col>
-						<u-col span="9">
-							<view class="uni-list">
-								<view class="uni-list-cell">
-									<view class="uni-list-cell-db"
-										style="border: rgb(236,236,236) solid 2px;text-align: center;">
-										<picker @change="bindPickerChangeB" :value="indexB" :range="builds">
-											<view class="uni-input">{{builds[indexB]}}</view>
-										</picker>
+			<view class="topInfo">
+				<view class="title">{{batchName}}</view>
+				<u-row gutter="20" customStyle="margin-bottom: 5%;margin-top: 5%">
+					<u-col span="6">
+						<u-row>
+							<u-col span="3">
+								<text>楼栋</text>
+							</u-col>
+							<u-col span="9">
+								<view class="uni-list">
+									<view class="uni-list-cell">
+										<view class="uni-list-cell-db"
+											style="border: rgb(236,236,236) solid 2px;text-align: center;">
+											<picker @change="bindPickerChangeB" :value="indexB" :range="builds">
+												<view class="uni-input">{{builds[indexB]}}</view>
+											</picker>
+										</view>
 									</view>
 								</view>
-							</view>
-						</u-col>
-					</u-row>
-				</u-col>
-				<u-col span="6">
-					<u-row>
-						<u-col span="5">
-							<text>完成情况</text>
-						</u-col>
-						<u-col span="7">
-							<view class="uni-list">
-								<view class="uni-list-cell">
-									<view class="uni-list-cell-db"
-										style="border: rgb(236,236,236) solid 2px;text-align: center;width: 85px;">
-										<picker @change="bindPickerChangeS" :value="indexS" :range="situation">
-											<view class="uni-input">{{situation[indexS]}}</view>
-										</picker>
+							</u-col>
+						</u-row>
+					</u-col>
+					<u-col span="6">
+						<u-row>
+							<u-col span="5">
+								<text>完成情况</text>
+							</u-col>
+							<u-col span="7">
+								<view class="uni-list">
+									<view class="uni-list-cell">
+										<view class="uni-list-cell-db"
+											style="border: rgb(236,236,236) solid 2px;text-align: center;width: 85px;">
+											<picker @change="bindPickerChangeS" :value="indexS" :range="situation">
+												<view class="uni-input">{{situation[indexS]}}</view>
+											</picker>
+										</view>
 									</view>
 								</view>
-							</view>
-						</u-col>
-					</u-row>
-				</u-col>
-			</u-row>
-			<u-collapse @change="change" @close="close" @open="open">
-				<u-collapse-item title="更多筛选" name="more select">
-					<text class="u-collapse-content"></text>
-				</u-collapse-item>
-			</u-collapse>
-			<view>
-				<u-grid :border="false" @click="">
-					<u-grid-item v-for="(baseListItem,baseListIndex) in baseList" :key="baseListIndex">
-						<text>{{baseListItem.name}}</text>
-						<text class="grid-text">{{baseListItem.count}}</text>
-					</u-grid-item>
-				</u-grid>
-				<u-toast ref="uToast" />
+							</u-col>
+						</u-row>
+					</u-col>
+				</u-row>
+				<u-collapse @change="change" @close="close" @open="open">
+					<u-collapse-item title="更多筛选" name="more select">
+						<text class="u-collapse-content"></text>
+					</u-collapse-item>
+				</u-collapse>
+				<view>
+					<u-grid :border="false" @click="">
+						<u-grid-item v-for="(baseListItem,baseListIndex) in baseList" :key="baseListIndex">
+							<text>{{baseListItem.name}}</text>
+							<text class="grid-text">{{baseListItem.count}}</text>
+						</u-grid-item>
+					</u-grid>
+					<u-toast ref="uToast" />
+				</view>
+				<u-divider customStyle="margin-bottom:0px"></u-divider>
 			</view>
-			<u-divider customStyle="margin-bottom:0px"></u-divider>
-		</view>
 		</u-sticky>
-	<!-- 	<view class="remind">
+		<!-- 	<view class="remind">
 			一键提醒该批次未完成同学
 		</view> -->
 		<view class="content">
@@ -94,8 +94,14 @@
 				</uni-card>
 			</view>
 		</view>
-		<u-loadmore :status="status" :loading-text="loadingText" :loadmore-text="loadmoreText"
-			:nomore-text="nomoreText" />
+		<view v-if="list!=0">
+			<u-loadmore :status="status" :loading-text="loadingText" :loadmore-text="loadmoreText"
+				:nomore-text="nomoreText" />
+		</view>
+		<view v-else>
+			<u-loadmore status="nomore" :loading-text="loadingText" :loadmore-text="loadmoreText"
+				:nomore-text="nomoreText" />
+		</view>
 		<u-back-top :scroll-top="scrollTop"></u-back-top>
 		<view v-if="list==total">
 			<view class="lastBlock">重庆理工大学</view>
@@ -162,16 +168,18 @@
 		},
 		onReachBottom() {
 			this.status = 'loading';
-			this.page = ++this.page;
-			console.log(this.total);
-			this.getData();
+			if (this.list < this.total) {
+				this.page = ++this.page;
+				console.log(this.total);
+				this.getData();
+			}
 			setTimeout(() => {
 				if (this.list < this.total) {
 					this.status = 'loading';
 				} else {
 					this.list = this.total;
 					this.status = 'nomore';
-				} 
+				}
 			}, 2000)
 		},
 		onPageScroll(e) {
@@ -189,6 +197,9 @@
 			}
 		},
 		onPullDownRefresh() {
+			this.page = 0;
+			this.list = 0;
+			this.stuInfo = [];
 			this.getData();
 		},
 		methods: {
@@ -205,27 +216,28 @@
 				this.page = 0;
 				this.list = 0;
 				this.stuInfo = [];
+				// 查询全部
 				if (this.indexS == 0) {
-					 this.stuStatus = '';
-					 this.getData();
-				}
-				if (this.indexS == 1) {
-					this.stuStatus = 0
+					this.stuStatus = '';
 					this.getData();
 				}
+				// 查询未完成
+				if (this.indexS == 1) {
+					this.stuStatus = 0;
+					this.getData();
+				}
+				// 查询已完成
 				if (this.indexS == 2) {
-					this.stuStatus = 1
+					this.stuStatus = 1;
 					this.getData();
 				}
 
 			},
 			getIndexB() {
 				for (let i = 0; i < this.builds.length; i++) {
-					if(this.builds[i] == this.building) {
+					if (this.builds[i] == this.building) {
 						this.indexB = i;
 						this.buildingId = this.buildings[this.indexB].id;
-						console.log("aaaa");
-						console.log(this.buildingId);
 						return;
 					}
 				}
@@ -293,7 +305,6 @@
 					currentPage: this.page + 1,
 					pageSize: this.pageSize
 				}
-				console.log(info);
 				TBAPI.getTestListByBatch(info).then(res => {
 					console.log(res);
 					if (res.statusCode == 404) {
@@ -304,7 +315,6 @@
 						return;
 					}
 					let temp = res.data.data.studentTestInfo;
-					console.log(temp);
 					for (let i = 0; i < temp.length; i++) {
 						this.stuInfo.push({
 							stuId: temp[i].studentId,
@@ -316,14 +326,25 @@
 							stuStatus: temp[i].status == 0 ? '未完成' : this.turnTime(temp[i].confirmTime)
 						})
 					}
-					this.total =  res.data.data.allNum;
-					this.incomplete = res.data.data.unCompleteNum;
-					this.complete = res.data.data.completeNum;
-					if(temp.length < this.pageSize) {
-						this.list = this.total;
-					}else{
-						this.list = this.list + this.pageSize;  
+					if (this.indexS == 1) {
+						this.incomplete = res.data.data.unCompleteNum;
+						this.total = this.incomplete;
+						this.complete = 0;
+					} else if (this.indexS == 2) {
+						this.complete = res.data.data.completeNum;
+						this.total = this.complete;
+						this.incomplete = 0;
+					} else {
+						this.total = res.data.data.allNum;
+						this.incomplete = res.data.data.unCompleteNum;
+						this.complete = res.data.data.completeNum;
 					}
+					if (temp.length < this.pageSize) {
+						this.list = temp.length;
+					} else {
+						this.list = this.list + this.pageSize;
+					}
+					console.log("1111");
 					this.getNumber();
 				}).catch(err => {
 					uni.showToast({
